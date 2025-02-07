9b5d01be0b00d8df663e92a9691bfe469a7db497
tomhwi01 <49177400+tomhwi01@users.noreply.github.com>
2025-02-07 19:56:27 +0800

    Add files

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/tomhwi01.github.io/",
+  plugins: [vue()],
+})
