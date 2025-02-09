23d27082e37b0def14715b2b372c490d33808dcd
tomhwi01 <49177400+tomhwi01@users.noreply.github.com>
2025-02-09 19:48:05 +0800

    Fix image

--- src/components/CategoriesPreview.vue
+++ src/components/CategoriesPreview.vue
@@ -5,7 +5,7 @@
       <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
         <div v-for="callout in callouts" :key="callout.name" class="group">
           <a :href="callout.href" target="_blank" rel="noopener noreferrer">
-            <img :src="callout.imageSrc" :alt="callout.imageAlt" class="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square" />
+            <img :src="callout.imageSrc" :alt="callout.imageAlt" type="image/jpeg" class="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square" />
             <h3 class="mt-6 text-base font-semibold text-white">
                 {{ callout.name }}
             </h3>
@@ -18,32 +18,36 @@
 </template>
 
 <script setup lang="ts">
+import siteImg from '../assets/site.jpeg'
+import webPageImg from '../assets/webpage.jpeg'
+import testResultImg from '../assets/test_result.jpeg'
+import sudokuImg from '../assets/sudoku.jpeg'
 const callouts = [
   {
     name: 'My Site',
     description: 'Built with Vue 3 + TypeScript + Vite + Tailwind CSS',
-    imageSrc: '/src/assets/site.jpg',
+    imageSrc: siteImg,
     imageAlt: 'My Site GitHub page',
     href: 'https://github.com/tomhwi01/tomhwi01.github.io',
   },
   {
     name: 'Dog Human Classifier',
     description: 'Built with Python + TensorFlow + Keras + Flask + HTML5',
-    imageSrc: '/src/assets/webpage.jpg',
+    imageSrc: webPageImg,
     imageAlt: 'Dog Human Classifier GitHub page',
     href: 'https://github.com/tomhwi01/dog-human-classifier',
   },
   {
     name: 'Audio Classifier',
     description: 'Built with Python + TensorFlow + Keras',
-    imageSrc: '/src/assets/test_result.jpg',
+    imageSrc: testResultImg,
     imageAlt: 'Audio Classifier GitHub page',
     href: 'https://github.com/tomhwi01/audio_classification',
   },
   {
     name: 'Sudoku Solver',
     description: 'Built with Java',
-    imageSrc: '/src/assets/sudoku.jpg',
+    imageSrc: sudokuImg,
     imageAlt: 'Sudoku Solver GitHub page',
     href: 'https://github.com/tomhwi01/sudoku',
   },
